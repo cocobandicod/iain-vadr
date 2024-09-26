baf7fa6f57d61610b94bbe345082ce2f544728a5
cocobandicod <allanamilie@gmail.com>
2024-09-26 23:03:55 +0800

    update 26/09/2024

--- vue/src/api/index.js
+++ vue/src/api/index.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 const Api = axios.create({
     //set default endpoint API
-    baseURL: 'http://localhost:8000'
+    //baseURL: 'http://localhost:8000'
+    baseURL: 'http://localhost/iain-vadr/public'
 })
 
 export default Api
